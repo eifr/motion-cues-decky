9bbc54d20cfee6c895a215ff5986eead2137fe01
eifr <eifr0980@gmail.com>
2024-08-29 08:26:17 +0300

    init

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import {
   PanelSection,
   PanelSectionRow,
   Navigation,
-  staticClasses
+  staticClasses,
 } from "@decky/ui";
 import {
   addEventListener,
@@ -11,10 +11,10 @@ import {
   callable,
   definePlugin,
   toaster,
-  // routerHook
-} from "@decky/api"
+  routerHook,
+} from "@decky/api";
 // import { call, callable } from "@decky/backend";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaShip } from "react-icons/fa";
 
 // import logo from "../assets/logo.png";
@@ -36,18 +36,12 @@ function Content() {
   return (
     <PanelSection title="Panel Section">
       <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={onClick}
-        >
+        <ButtonItem layout="below" onClick={onClick}>
           {result || "Add two numbers via Python"}
         </ButtonItem>
       </PanelSectionRow>
       <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => startTimer()}
-        >
+        <ButtonItem layout="below" onClick={() => startTimer()}>
           {"Start Python timer"}
         </ButtonItem>
       </PanelSectionRow>
@@ -71,6 +65,63 @@ function Content() {
       </PanelSectionRow>
     </PanelSection>
   );
+}
+
+const GlobalComponent = () => {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [z, setZ] = useState(0);
+  useEffect(() => {
+    const registration =
+      window.SteamClient.Input.RegisterForControllerStateChanges(
+        (changes: any[]) => {
+          for (const inputs of changes) {
+            console.log(
+              "controller state flGravityVectorX:",
+              inputs["flGravityVectorX"]
+            );
+
+            // logic
+            setX(inputs["flGravityVectorX"]);
+            setY(inputs["flGravityVectorY"]);
+            setZ(inputs["flGravityVectorZ"]);
+          }
+        }
+      );
+
+    return () => {
+      console.log("unregister controller state changes");
+      registration.unregister();
+    };
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "fixed",
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        top: "0",
+        left: "0",
+        transform: `translateX(${100*x}px) translateY(${100*y}px) translateZ(${100*z}px)`,
+        transition: "transform 0.1s linear",
+        pointerEvents: "none",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "red",
+          borderRadius: "10px",
+          padding: "10px",
+        }}
+      >
+        Global component 2
+      </div>
+    </div>
+  );
 };
 
 export default definePlugin(() => {
@@ -78,24 +129,28 @@ export default definePlugin(() => {
   //   exact: true,
   // });
   // console.log("init plugin", call, callable)
-  const listener = addEventListener<[
-    test1: string,
-    test2: boolean,
-    test3: number
-  ]>("test_event", (test1, test2, test3) => {
-    console.log("Template got event", test1, test2, test3)
+
+  routerHook.addGlobalComponent("BrightnessBar", GlobalComponent);
+
+  const listener = addEventListener<
+    [test1: string, test2: boolean, test3: number]
+  >("test_event", (test1, test2, test3) => {
+    console.log("Template got event", test1, test2, test3);
     toaster.toast({
       title: "template got event",
-      body: `${test1}, ${test2}, ${test3}`
+      body: `${test1}, ${test2}, ${test3}`,
     });
   });
+
   return {
     title: <div className={staticClasses.Title}>API v2 Example Plugin</div>,
     content: <Content />,
     icon: <FaShip />,
     onDismount() {
-      console.log("Unloading")
+      console.log("Unloading");
       removeEventListener("test_event", listener);
+      routerHook.removeGlobalComponent("BrightnessBar");
+
       // serverApi.routerHook.removeRoute("/decky-plugin-test");
     },
   };
